df99446da71090674b3a445067548ca06b662fb8
woodminus <160528767+woodminus@users.noreply.github.com>
2024-05-08 18:12:00 +0000

    Add init

--- TextGeneration.py
+++ TextGeneration.py
@@ -130,3 +130,116 @@ def rnn(x, weight, bias, len_unique_chars):
     x = tf.transpose(x, [1, 0, 2])
     x = tf.reshape(x, [-1, len_unique_chars])
     x = tf.split(x, max_len, 0)
+
+    cell = tf.contrib.rnn.BasicLSTMCell(num_units, forget_bias=1.0)
+    outputs, states = tf.contrib.rnn.static_rnn(cell, x, dtype=tf.float32)
+    prediction = tf.matmul(outputs[-1], weight) + bias
+    return prediction
+
+def sample(predicted):
+    '''
+     helper function to sample an index from a probability array
+    '''
+    exp_predicted = np.exp(predicted/temperature)
+    predicted = exp_predicted / np.sum(exp_predicted)
+    probabilities = np.random.multinomial(1, predicted, 1)
+    return probabilities
+
+def run(train_data, target_data, unique_chars, len_unique_chars):
+    '''
+     main run function
+    '''
+    x = tf.placeholder("float", [None, max_len, len_unique_chars], name ="Input")
+    y = tf.placeholder("float", [None, len_unique_chars], name = "Output")
+    weight = tf.Variable(tf.random_normal([num_units, len_unique_chars]))
+    bias = tf.Variable(tf.random_normal([len_unique_chars]))
+
+    prediction = rnn(x, weight, bias, len_unique_chars)
+    softmax = tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y)
+    cost = tf.reduce_mean(softmax)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(cost)
+
+
+    import glob
+    if glob.glob(SAVE_PATH + '*.meta'):
+        tf.reset_default_graph()
+        imported_meta = tf.train.import_meta_graph(glob.glob(SAVE_PATH + '*.meta')[0])
+        sess=tf.Session()
+        imported_meta.restore(sess, tf.train.latest_checkpoint(SAVE_PATH))
+        print (" restoring an old model and training it further ")
+
+        x = sess.graph.get_tensor_by_name("Input:0")
+        y = sess.graph.get_tensor_by_name("Output:0")
+        prediction = tf.get_collection("prediction")[0]
+        optimizer = tf.get_collection("optimizer")[0]
+
+    else:
+
+        init_op = tf.global_variables_initializer()
+        sess = tf.Session()
+        sess.run(init_op)
+        print("Building model from scratch!")
+
+    saver = tf.train.Saver(max_to_keep=4, keep_checkpoint_every_n_hours=1, save_relative_paths=True)
+    num_batches = int(len(train_data)/batch_size)
+
+    for i in range(epoch):
+        print "----------- Epoch {0}/{1} -----------".format(i+1, epoch)
+        count = 0
+        for _ in range(num_batches):
+            train_batch, target_batch = train_data[count:count+batch_size], target_data[count:count+batch_size]
+            count += batch_size
+            sess.run([optimizer] ,feed_dict={x:train_batch, y:target_batch})
+            tf.add_to_collection("optimizer", optimizer)
+
+        #get on of training set as seed
+        seed = train_batch[:1:]
+
+        #to print the seed 40 characters
+        seed_chars = ''
+        for each in seed[0]:
+                seed_chars += unique_chars[np.where(each == max(each))[0][0]]
+        print "Seed:", seed_chars
+
+        #predict next 500 characters
+        for i in range(500):
+            if i > 0:
+                remove_fist_char = seed[:,1:,:]
+                seed = np.append(remove_fist_char, np.reshape(probabilities, [1, 1, len_unique_chars]), axis=1)
+            predicted = sess.run([prediction], feed_dict = {x:seed})
+            tf.add_to_collection("prediction", prediction)
+            predicted = np.asarray(predicted[0]).astype('float64')[0]
+            probabilities = sample(predicted)
+            predicted_chars = unique_chars[np.argmax(probabilities)]
+            seed_chars += predicted_chars
+        print 'Result:', seed_chars
+        print 'Corrected:', correct_text_generic(seed_chars)
+    ui = True
+    while ui == True:
+        seed_chars = raw_input("Enter a seed: ")
+        for i in range(280):
+            if i > 0:
+                remove_fist_char = seed[:, 1:, :]
+                seed = np.append(remove_fist_char, np.reshape(probabilities, [1, 1, len_unique_chars]), axis=1)
+            predicted = sess.run([prediction], feed_dict={x: seed})
+            predicted = np.asarray(predicted[0]).astype('float64')[0]
+            probabilities = sample(predicted)
+            predicted_chars = unique_chars[np.argmax(probabilities)]
+            seed_chars += predicted_chars
+        # print 'Result:', seed_chars
+        print 'Corrected:', correct_text_generic(seed_chars)
+        action = raw_input("Do you want to try another seed? (yes=y, no=n)?: ")
+        if action != "y":
+            ui = False
+    save_path = saver.save(sess, SAVE_PATH, global_step=10)
+    print("Model saved in file: %s" % save_path)
+    sess.close()
+
+    tf.reset_default_graph()
+
+if __name__ == "__main__":
+    text = read_data('RilkeLyrik.txt')
+    text = re.sub(r'[0-9]+', '', text)
+    train_data, target_data, unique_chars, len_unique_chars = featurize(text)
+    tf.reset_default_graph()
+    run(train_data, target_data, unique_chars, len_unique_chars)
---
+++ W2V-TF2.py
@@ -0,0 +1,22 @@
+import os
+import urllib
+import tensorflow as tf
+import zipfile
+import collections
+import numpy as np
+import io
+
+def maybe_download(filename, url, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+            'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
+url = 'h
