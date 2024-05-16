bfacf933cb755cb39cacc6c0ae0f94606d632e31
woodminus <160528767+woodminus@users.noreply.github.com>
2024-05-16 15:37:17 +0000

    drm/i915: Reset last_retired_head when resetting ring

--- skipgramTF-2.py
+++ skipgramTF-2.py
@@ -37,4 +37,41 @@ def build_dataset(words, n_words):
         if word in dictionary:
             index = dictionary[word]
         else:
-            index = 0  # dictionar
+            index = 0  # dictionary['UNK']
+            unk_count += 1
+        data.append(index)
+    count[0][1] = unk_count
+    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reversed_dictionary
+
+
+spm.SentencePieceTrainer.Train('--input={} --model_prefix=m --vocab_size={}'.format(filename, sub_size))
+
+sp = spm.SentencePieceProcessor()
+sp.load('m.model')
+
+print(sp.encode_as_pieces('this is a test.'))
+
+vocabulary = sp.encode_as_pieces(text)
+
+data, count, dictionary, reverse_dictionary = build_dataset(vocabulary, vocab_size)
+
+
+sampling_table = tf.keras.preprocessing.sequence.make_sampling_table(vocab_size)
+couples, labels = tf.keras.preprocessing.sequence.skipgrams(data, vocab_size, window_size=window_size, sampling_table=sampling_table)
+word_target, word_context = zip(*couples)
+word_target = np.array(word_target, dtype="int32")
+word_context = np.array(word_context, dtype="int32")
+
+print(couples[:10], labels[:10])
+
+# create some input variables
+input_target = tf.keras.Input((1,))
+input_context = tf.keras.Input((1,))
+
+embedding = tf.keras.layers.Embedding(vocab_size, vector_dim, input_length=1, name='embedding')
+
+target = embedding(input_target)
+target = tf.keras.layers.Reshape((vector_dim, 1))(target)
+context = embedding(input_context)
+context = tf.keras.layers.R
